5e98b9c2b8285fa50634e4a456728b219fc612b3
twoweeksbro <twoweeksbro@gmail.com>
2024-05-13 22:47:27 +0900

    log in n out

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -19,10 +19,14 @@ def create_app():
 
 
     # Blueprint
-    from .views import main_views, question_views, answer_views
+    from .views import main_views, question_views, answer_views, auth_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
+    app.register_blueprint(auth_views.bp)
 
+    # Filter
+    from .filter import format_datetime
+    app.jinja_env.filters['datetime'] = format_datetime
     return app
 
