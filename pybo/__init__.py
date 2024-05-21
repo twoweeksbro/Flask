6d9d0756d125e527764999004f61f8586b0963f3
twoweeksbro <twoweeksbro@gmail.com>
2024-05-21 23:23:10 +0900

    404 error

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -14,6 +14,9 @@ naming_convention = {
 }
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
+
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 def create_app():
 
@@ -43,5 +46,7 @@ def create_app():
 
     # Markdown
 
+    # Error Page
+    app.register_error_handler(404, page_not_found)
     return app
 
